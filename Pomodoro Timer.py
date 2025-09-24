4e98e5dfd720c4c42b85ea6e703bffc09a0e5988
Nolen Jackson <nolenj@ou.edu>
2025-09-24 18:14:25 -0500

    Commented my name

--- Pomodoro Timer.py
+++ Pomodoro Timer.py
@@ -33,3 +33,5 @@ if __name__ == "__main__":
     pomodoro_timer(1,1,2,sessions)                                                      #starts timer w/ default testing values 
 
     #HI terrry!
+
+    # Nolen was here
