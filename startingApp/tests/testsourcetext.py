492d4ac2152bb75878413291c54093b576f17ccc
plain-bagels <143725251+plain-bagels@users.noreply.github.com>
2025-10-31 01:08:08 -0500

    Update testsourcetext.py

--- startingApp/tests/testsourcetext.py
+++ startingApp/tests/testsourcetext.py
@@ -1,18 +1,17 @@
 import unittest
 from sourcetext_validator import validate_sourcetext
-from deck_validator import validate_deck
 
 
 class TestSourceText(unittest.TestCase):
     def test_valid_source_text(self):
-        self.assertTrue(validate_sourcetext("a"))
+        self.assertTrue(validate_sourcetext("a"))    # test a short text, 1 char
 
     def test_invalid_sourcetext(self):
-        self.assertFalse(validate_sourcetext(""))
+        self.assertFalse(validate_sourcetext(""))    # test no text
 
     def test_valid_long_sourceText(self):
         long_source = "a" * 10000
-        self.assertTrue(validate_sourcetext(long_source))
+        self.assertTrue(validate_sourcetext(long_source))    # test a long test, user should be able to use a text as long as they want
 
 
 if __name__ == '__main__':
